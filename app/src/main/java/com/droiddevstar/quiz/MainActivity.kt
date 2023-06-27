158a4d39dd2097bb1c9eaf38ee6662089d9accc5
dstarchevskyy <droiddevstar@gmail.com>
2023-06-27 13:48:34 +0300

    remove unused code

--- app/src/main/java/com/droiddevstar/quiz/MainActivity.kt
+++ app/src/main/java/com/droiddevstar/quiz/MainActivity.kt
@@ -5,20 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.lifecycleScope
 import com.arkivanov.decompose.defaultComponentContext
-import com.droiddevstar.quiz.retrofit.JokeApi
-import com.droiddevstar.quiz.retrofit.JokesApiImpl
-import com.droiddevstar.quiz.retrofit.JokesRepositoryImpl
-import com.droiddevstar.quiz.retrofit.RetrofitApiFactory
-import com.droiddevstar.quiz.retrofit.RetrofitJokeApi
 import com.droiddevstar.quiz.root.DefaultRootComponent
 import com.droiddevstar.quiz.root.RootContent
 import com.droiddevstar.quiz.ui.theme.QuizTheme
@@ -26,25 +16,16 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import kotlin.coroutines.CoroutineContext
 
 @AndroidEntryPoint
-class MainActivity() : ComponentActivity(), CoroutineScope {
+class MainActivity : ComponentActivity(), CoroutineScope {
     private lateinit var job: Job
 
-    lateinit var jokeApi: JokeApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         job = Job()
-
-        val retrofitJokeApi = RetrofitApiFactory.jokeApiService
-        jokeApi = JokesApiImpl(retrofitJokeApi)
 
         // Always create the root component outside Compose on the main thread
         val root =
@@ -61,10 +42,6 @@ class MainActivity() : ComponentActivity(), CoroutineScope {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     RootContent(component = root, modifier = Modifier.fillMaxWidth())
-
-//                    Button(onClick = { getJoke() }) {
-//                        Text(text = "My text")
-//                    }
                 }
             }
         }
@@ -73,31 +50,4 @@ class MainActivity() : ComponentActivity(), CoroutineScope {
     override val coroutineContext: CoroutineContext
         get() =  Dispatchers.Main + job
 
-    private fun getJoke() {
-        launch {
-            JokesRepositoryImpl(jokeApi).getJoke()
-                .catch {
-                    println("@@@catch: $it")
-                }
-                .collect {
-                    println("@@@collect: $it")
-                }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuizTheme {
-        Greeting("Android")
-    }
 }
